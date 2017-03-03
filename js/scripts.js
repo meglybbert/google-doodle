43ef5159c055556360f5d01c256ce3a220357346
meglybbert <meglybbert@eagles.ewu.edu>
2017-03-02 23:45:53 -0800

    Added vendor prefixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 var google = $('svg').drawsvg();
 
 //Random Number Generator
-var limit = 3;
-var randNum = Math.floor(Math.random() * limit);
-console.log(randNum);
+//var limit = 3;
+//var randNum = Math.floor(Math.random() * limit);
+//console.log(randNum);
 
-//click on stage
-$('#stage').click(function(){
-  //animate stroke drawing
-  google.drawsvg('animate');
-  //grab the icons
-  $target = $('.cls-2.icon');
-  //apply randomized animation and color to the icons and reset the click event results
-  $target.removeClass('pulse'+randNum);
-  setTimeout("$target.addClass('pulse'+randNum);",100)  
+//Click on stage
+$('#stage').click(function () {
+    //animate stroke drawing
+    google.drawsvg('animate');
+    //testing
+    console.log('Still working');
+    //grab the icons
+    $target = $('.cls-2.icon');
+    //apply randomized animation/color to the icons and reset 
+    $target.removeClass('pulse' + randNum);
+    setTimeout("$target.addClass('pulse' + randNum)", 100);
 });
-
