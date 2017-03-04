04678cd275d72c02870187ad0a7ec052729e57cd
meglybbert <meglybbert@eagles.ewu.edu>
2017-03-03 18:13:09 -0800

    Trying to get stuff straightened out

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,13 @@ var limit = 3;
 var randNum = Math.floor(Math.random() * limit);
 console.log(randNum);
 
-//Click on stage
-$('#stage').click(function () {
-    //animate stroke drawing
-    google.drawsvg('animate');
-    //testing
-    console.log('Still working');
-    //grab the icons
-    $target = $('.cls-2.icon');
-    //apply randomized animation/color to the icons and reset 
-    $target.removeClass('pulse' + randNum);
-    setTimeout("$target.addClass('pulse'+randNum);", 100);
+//click on stage
+$('#stage').click(function() {  
+  //animate stroke drawing
+  google.drawsvg('animate');
+  //grab the icons
+  $target = $('.icon');
+  //apply randomized animation/color to the icons and reset 
+  $target.removeClass('pulse' + randNum);
+  setTimeout("$target.addClass('pulse'+randNum);", 100)
 });
